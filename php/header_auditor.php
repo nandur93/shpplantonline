410fcb80452659f3471ba30fbebcbf915844873c
nandur93 <nandang.dhe@gmail.com>
2020-02-29 02:47:13 +0700

    delete error file php

--- php/header_auditor.php
+++
@@ -1,8 +0,0 @@
-<?php
-$link = mysqli_connect("localhost", "root", "admin", "shpplantonline");
-$id   = $_GET['id'];
-$sname = $_GET['sname'];
-$auditeename = $_GET['auditee'];
-$query_mysql = mysqli_query($link, "SELECT * FROM tbl_auditor WHERE tbl_auditor.ID ='$id', tbl_auditor.adtr_snmae = '$sname", tbl_auditor.adtee_name) or die(mysql_error());
-while ($data = mysqli_fetch_array($query_mysql)) {
-?>
